25c644be2c8a98091d732eeac5cb6aa248d3630e
Junseung-Ock <junseung0305@kyonggi.ac.kr>
2026-03-16 21:27:19 +0900

    p020 우선순위 큐를 사용하여 구현

---
+++ src/codingtest/p020.java
@@ -0,0 +1,23 @@
+package codingtest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class p020 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0; i<N; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+        for(int i=0; i<N; i++) {
+            sb.append(pq.poll()).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
